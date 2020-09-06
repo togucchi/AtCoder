f94ad828333bb8c90e3a656a2b8e2d24a73cfd1b
togucchi <fmacro00@gmail.com>
2020-09-06 20:56:23 +0900

    入力用のstatic関数作成

---
+++ AtCoder/Lib/Reader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Lib
+{
+    public static class Reader
+    {
+        /// <summary>
+        /// ReadLine.
+        /// </summary>
+        /// <returns></returns>
+        public static string ReadLine()
+        {
+            // 文字列の入力
+            return Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Read num.
+        /// </summary>
+        /// <returns></returns>
+        public static long ReadNum()
+        {
+            return long.Parse(Console.ReadLine());
+        }
+
+        /// <summary>
+        /// Read string array.s
+        /// </summary>
+        /// <returns></returns>
+        public static string[] ReadStrArray()
+        {
+            return Console.ReadLine().Split(' ');
+        }
+
+        /// <summary>
+        /// Read num array.
+        /// </summary>
+        /// <returns></returns>
+        public static long[] ReadNumArray()
+        {
+            return Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+        }
+    }
+}
